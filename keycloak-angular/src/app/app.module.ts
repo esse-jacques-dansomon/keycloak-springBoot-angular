3e0dc3bf5508315acc094cef127eea217e8100a4
Esse Dansomon <essedansomon@gmail.com>
2023-03-04 00:18:25 +0000

    add keycloak module and provider

--- keycloak-angular/src/app/app.module.ts
+++ keycloak-angular/src/app/app.module.ts
@@ -1,8 +1,11 @@
-import { NgModule } from '@angular/core';
+import {APP_INITIALIZER, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {KeycloakAngularModule, KeycloakService} from "keycloak-angular";
+import {HttpClientModule} from "@angular/common/http";
+import {initializer} from "../utils/app-init";
 
 @NgModule({
   declarations: [
@@ -10,9 +13,19 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    KeycloakAngularModule, // add keycloak-angular module
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    // add keycloak-angular initializer
+    {
+      provide: APP_INITIALIZER,
+      useFactory: initializer,
+      deps: [KeycloakService],
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
